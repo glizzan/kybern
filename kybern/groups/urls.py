6ff0dc58ed8aaf9c000a8b555c51b30c3eec3f14
Shauna Gordon-McKeon <shaunagm@gmail.com>
2020-03-10 19:56:43 -0400

    Fix bootstrap-vue scope; add ability to add roles

--- kybern/groups/urls.py
+++ kybern/groups/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.GroupListView.as_view(), name='group_list'),
     path('create/', views.GroupCreateView.as_view(), name='group_create'),
     path('<int:pk>/', views.GroupDetailView.as_view(), name='group_detail'),
+    path('add_role/<int:target>/<str:role_name>/', views.add_role, name='add_role_to_group'),
 ]
--- kybern/groups/views.py
+++ kybern/groups/views.py
@@ -1,8 +1,10 @@
 from django.views import generic
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
+
 from django.urls import reverse
 
 from concord.actions.client import ActionClient
+from concord.communities.client import CommunityClient
 
 from .models import Group
 
@@ -19,8 +21,11 @@ class GroupDetailView(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         actionClient = ActionClient(actor=self.request.user, 
-            target=self.object)   
+            target=self.object)
         context['actions'] = actionClient.get_action_history_given_target()
+        communityClient = CommunityClient(actor=self.request.user, 
+            target=self.object)
+        context['roles'] = communityClient.get_role_names()
         return context
 
 class GroupCreateView(generic.edit.CreateView):
@@ -30,7 +35,22 @@ class GroupCreateView(generic.edit.CreateView):
         'foundational_permission_enabled']
 
     def form_valid(self, form):
+        # FIXME: should this be so fiddly?
         self.object = form.save(commit=False)
         self.object.owner = self.request.user
+        self.object.roles.initialize_with_creator(creator=self.request.user.pk)
         self.object.save()
         return HttpResponseRedirect(reverse('group_detail', kwargs={'pk': self.object.pk}))
+
+
+# helper methods, likely to be moved to concord
+
+def add_role(request, target, role_name):
+    communityClient = CommunityClient(actor=request.user)
+    target = communityClient.get_community(community_pk=target)
+    communityClient.set_target(target=target)
+    action, result = communityClient.add_role(role_name=role_name)
+    return JsonResponse({
+        "action_status": "success" if action.resolution.status == "implemented" else "error",
+        "action_log": action.resolution.log
+    })
