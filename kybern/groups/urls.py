fa005fda5c21438a987da90ea6e8933da6092c3c
Shauna Gordon-McKeon <shaunagm@gmail.com>
2020-03-19 14:31:43 -0400

    Some updates to vue governance interface

--- kybern/groups/urls.py
+++ kybern/groups/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     path('', views.GroupListView.as_view(), name='group_list'),
     path('create/', views.GroupCreateView.as_view(), name='group_create'),
     path('<int:pk>/', views.GroupDetailView.as_view(), name='group_detail'),
-    path('add_role/<int:target>/<str:role_name>/', views.add_role, name='add_role_to_group'),
+    path('add_role/<int:target>/', views.add_role, name='add_role_to_group'),
+    path('add_people_to_role/<int:target>/', views.add_people_to_role, name='add_people_to_role'),
+    path('add_members/<int:target>/', views.add_members, name='add_members'),
+    path('get_permissions_given_role/<int:target>/', views.get_permissions_given_role, name='get_permissions_given_role'),
+    path('add_permission/<int:target>/', views.add_permission, name='add_permission'),
+    path('add_condition/<int:target>/', views.add_condition, name='add_condition'),
+    path('update_permission/<int:target>/', views.update_permission, name='update_permission'),
+    path('update_condition/<int:target>/', views.update_condition, name='update_condition'),
+
 ]
--- kybern/groups/views.py
+++ kybern/groups/views.py
@@ -1,10 +1,14 @@
 from django.views import generic
 from django.http import HttpResponseRedirect, JsonResponse
+import json
 
 from django.urls import reverse
 
+from django.contrib.auth.models import User
 from concord.actions.client import ActionClient
 from concord.communities.client import CommunityClient
+from concord.permission_resources.client import PermissionResourceClient
+from concord.conditionals.client import PermissionConditionalClient
 
 from .models import Group
 
@@ -20,13 +24,21 @@ class GroupDetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # Actions
         actionClient = ActionClient(actor=self.request.user, 
             target=self.object)
         context['actions'] = actionClient.get_action_history_given_target()
-        communityClient = CommunityClient(actor=self.request.user, 
-            target=self.object)
-        context['roles'] = communityClient.get_role_names()
-        return context
+        # Role/Member info
+        communityClient = CommunityClient(actor=self.request.user, target=self.object)
+        context['username_map'] = { person.pk : person.username for person in User.objects.all() }
+        context['current_members'] = [member.pk for member in communityClient.get_members()]
+        context['potential_members'] = list(set(context['username_map'].keys()).difference(set(context['current_members'])))
+
+        # Role Info
+        context['roles'] = [ { 'role_name': role_name, 'current_members': role_data } 
+                                        for role_name, role_data in communityClient.get_custom_roles().items() ]
+        return context        
+
 
 class GroupCreateView(generic.edit.CreateView):
     model = Group
@@ -43,14 +55,286 @@ class GroupCreateView(generic.edit.CreateView):
         return HttpResponseRedirect(reverse('group_detail', kwargs={'pk': self.object.pk}))
 
 
-# helper methods, likely to be moved to concord
+#####################################################
+### Helper methods, likely to be moved to concord ###
+#####################################################
 
-def add_role(request, target, role_name):
+
+def add_role(request, target):
+
+    request_data = json.loads(request.body.decode('utf-8'))
+
     communityClient = CommunityClient(actor=request.user)
     target = communityClient.get_community(community_pk=target)
     communityClient.set_target(target=target)
-    action, result = communityClient.add_role(role_name=role_name)
+    
+    action, result = communityClient.add_role(role_name=request_data['role_name'])
+    
     return JsonResponse({
         "action_status": "success" if action.resolution.status == "implemented" else "error",
         "action_log": action.resolution.log
     })
+
+
+def add_members(request, target):
+
+    request_data = json.loads(request.body.decode('utf-8'))
+
+    communityClient = CommunityClient(actor=request.user)
+    target = communityClient.get_community(community_pk=target)
+    communityClient.set_target(target=target)
+
+    action, result = communityClient.add_members(member_pk_list=request_data["people_pks"])
+
+    return JsonResponse({ "action_status": "success" if action.resolution.status == "implemented" else "error",
+                        "action_log": action.resolution.log })
+
+
+def add_people_to_role(request, target):
+
+    request_data = json.loads(request.body.decode('utf-8'))
+
+    communityClient = CommunityClient(actor=request.user)
+    target = communityClient.get_community(community_pk=target)
+    communityClient.set_target(target=target)
+
+    action, result = communityClient.add_people_to_role(role_name=request_data['role_name'], 
+        people_to_add=request_data['people_pks'])
+
+    return JsonResponse({ "action_status": "success" if action.resolution.status == "implemented" else "error",
+                          "action_log": action.resolution.log })
+
+
+###########################
+### Stuff for edit role ###
+###########################
+
+
+def ready_condition_fields_for_display(condition):
+
+    # NOTE: object type for non-existing data is Condition (as in ApprovalCondition) class not instance
+    existing_data = True if condition.__class__.__name__ == "ConditionTemplate" else False
+    condition_fields = condition.get_condition_type_class().get_configurable_fields() if existing_data else condition.get_configurable_fields()
+
+    field_dict = []
+    for field_name, field in condition_fields.items():
+        # field.field syntax is because field object is actually DeferredAttribute wrapper
+        required_text = "required" if field.field.blank else ""
+        if existing_data:
+            value = json.loads(condition.condition_data)[field_name]
+        else:
+            value = field.field.default
+        field_dict.append({ 'name': field_name, 'type': field.field.__class__.__name__, 
+            'required': required_text,  'value': value })
+
+    return field_dict
+
+
+def ready_permission_fields_for_display(permission):
+
+    # NOTE: object type for none-existing data is State Change Object class not instance
+    existing_data = True if permission.__class__.__name__ == "PermissionsItem" else False
+    permission_fields = permission.get_configuration() if existing_data else permission.get_configurable_fields()
+
+    fields = []
+    if existing_data:
+        for key, value in permission_fields.items():
+            fields.append({'name': key, 'value': value})
+    else:
+        for field in permission_fields:
+            fields.append({'name': field, 'value': None})
+
+    return fields
+
+
+def get_permissions_given_role(request, target):
+
+    request_data = json.loads(request.body.decode('utf-8'))
+
+    permissionClient = PermissionResourceClient(actor=request.user)
+    communityClient = CommunityClient(actor=request.user)
+    target = communityClient.get_community(community_pk=target)
+    permissionClient.set_target(target=target)
+
+    ### Prep permissions
+
+    # Get existing permissions
+    existing_permissions = permissionClient.get_permissions_associated_with_role(role_name=request_data['role_name'])
+    
+    # Serialize them for vue
+    serialized_existing_permissions = []
+    for permission in existing_permissions:
+        serialized_existing_permissions.append({ "id": permission.pk, 
+            "name": permission.full_description(), "display": permission.full_description(), 
+            "change_type": permission.change_type,
+            "configuration": ready_permission_fields_for_display(permission) })
+
+    # Get permission options
+    settable_permissions = permissionClient.get_settable_permissions(return_format="state_change_objects")
+    permission_options = [ { "value": perm.get_change_type(), "text": perm.description } for perm in settable_permissions ]
+
+    # Create permission configuration options
+    permission_configuration = {}
+    for sco in settable_permissions:
+        permission_configuration[sco.get_change_type()] = ready_permission_fields_for_display(sco)                      
+           
+    ### Prep conditions
+    
+    # Get existing conditions
+    conditionalClient = PermissionConditionalClient(actor=request.user)
+    existing_conditions = conditionalClient.get_conditions_given_targets(
+            target_pks=[permission.pk for permission in existing_permissions])
+    
+    # Serialize them for vue
+    serialized_existing_conditions = {}    
+    for obj in existing_conditions:
+        serialized_existing_conditions.update({ obj.conditioned_object_id : 
+            { 'name' : obj.get_condition_type_class().descriptive_name, 'id': obj.pk,
+            'configuration': ready_condition_fields_for_display(obj) } })
+
+    # Get condition options
+    settable_conditions = conditionalClient.get_possible_conditions()
+    condition_options = [ { 'value': cond.__name__, 'text': cond.descriptive_name } for cond in settable_conditions ]
+
+    # Create condition configuration 
+    condition_configuration = { }
+    for condition in settable_conditions:
+        condition_configuration.update({ condition.__name__ : 
+            { 'condition_name' : condition.descriptive_name, 
+            'configuration': ready_condition_fields_for_display(condition) }  })
+     
+    return JsonResponse({ 
+        "existing_permissions": serialized_existing_permissions,
+        "permission_options": permission_options,
+        "permission_configuration": permission_configuration,
+        "existing_conditions": serialized_existing_conditions,
+        "condition_options": condition_options,
+        "condition_configuration": condition_configuration
+    })
+
+
+def reformat_permission_data(permission_data):
+    result = { field['name'] : field['value'] for field in permission_data }
+    return result
+
+
+def add_permission(request, target):
+
+    request_data = json.loads(request.body.decode('utf-8'))
+    permission_type = request_data.get("permission_type", None)
+    permission_actors = request_data.get("permission_actors", None)
+    permission_roles = request_data.get("permission_roles", None)
+    permission_configuration = request_data.get("permission_configuration", None)
+
+    reformatted_permission_data = reformat_permission_data(permission_configuration)
+
+    communityClient = CommunityClient(actor=request.user)
+    target = communityClient.get_community(community_pk=target)
+    permissionClient = PermissionResourceClient(actor=request.user, target=target)
+
+    action, result = permissionClient.add_permission(permission_type=permission_type, 
+        permission_actors=permission_actors, permission_roles=permission_roles, 
+        permission_configuration=reformatted_permission_data)
+
+    if action.resolution.status == "implemented":
+        permission_info = {"id": result.pk, "name": result.get_name(), 
+            "display": result.full_description(), "change_type": result.change_type, 
+            "configuration": ready_permission_fields_for_display(result) }
+    else:
+        permission_info = None
+
+    return JsonResponse({ "action_status": "success" if action.resolution.status == "implemented" else "error",
+                          "action_log": action.resolution.log,
+                          "permission_info": permission_info })
+
+
+def update_permission(request, target):
+
+    request_data = json.loads(request.body.decode('utf-8'))
+    permission_id = request_data.get("permission_id", None)
+    permission_configuration = request_data.get("permission_configuration", None)
+
+    reformatted_permission_data = reformat_permission_data(permission_configuration)
+
+    permissionClient = PermissionResourceClient(actor=request.user)
+    target_permission = permissionClient.get_permission(pk=permission_id)
+    permissionClient.set_target(target=target_permission)
+    actions = permissionClient.update_configuration(configuration_dict=reformatted_permission_data, 
+        permission=target_permission)
+
+    action_log = ""
+    action_status = "success"
+    for action in actions:
+        if action.resolution.status != "implemented":
+            action_status = "error"
+            action_log += action.resolution.log
+
+    permission = permissionClient.get_permission(pk=permission_id)  # get refreshed version
+
+    return JsonResponse({ "action_status": action_status, "action_log": action_log,
+        "new_display_name" : permission.full_description() })
+
+
+def reformat_condition_data(condition_data):
+    if type(condition_data) == str:
+        condition_data = json.loads(condition_data)
+    reformatted_condition_data = {}
+    for item in condition_data:
+        if item['type'] == "BooleanField":
+            item['value'] = True if item['value'] == "true" else item['value']
+            item['value'] = False if item['value'] == "false" else item['value']
+        if item['type'] in ["FloatField", "IntegerField"]: # Should probably treat floatfield differently
+            item['value'] = int(item['value'])  # FIXME: implement try/catch and return as error?
+        reformatted_condition_data.update({ item['name'] : item['value'] })
+    print(json.dumps(reformatted_condition_data))
+    return json.dumps(reformatted_condition_data)
+
+
+def add_condition(request, target):
+
+    request_data = json.loads(request.body.decode('utf-8'))
+    condition_type = request_data.get("condition_type", None)
+    condition_data = request_data.get("condition_data", None)
+    permission_data = request_data.get("permission_data", None)   
+    target_permission_id = request_data.get("target_permission_id", None)
+
+    reformatted_condition_data = reformat_condition_data(condition_data)
+
+    target_permission = PermissionResourceClient(actor=request.user).get_permission(pk=target_permission_id)
+    conditionalClient = PermissionConditionalClient(actor=request.user, target=target_permission)
+
+    action, result = conditionalClient.add_condition(condition_type=condition_type.lower(),
+        condition_data=reformatted_condition_data, permission_data=permission_data)
+
+    if action.resolution.status == "implemented":
+        condition_info = {"name": result.get_condition_type_class().descriptive_name, 
+            "configuration": ready_condition_fields_for_display(result), "id": result.pk }
+    else:
+        condition_info = None 
+
+    return JsonResponse({ "action_status": "success" if action.resolution.status == "implemented" else "error",
+                          "action_log": action.resolution.log,
+                          "condition_info": condition_info })
+
+
+
+def update_condition(request, target):
+
+    request_data = json.loads(request.body.decode('utf-8'))
+    permission_id = request_data.get("permission_id", None)
+    condition_id = request_data.get("condition_id", None)
+    configuration_data = request_data.get("condition_configuration", None)
+
+    reformatted_condition_data = reformat_condition_data(configuration_data)
+
+    #FIXME: the client call below doesn't require target
+    permissionClient = PermissionResourceClient(actor=request.user)
+    target_permission = permissionClient.get_permission(pk=permission_id)
+
+    conditionClient = PermissionConditionalClient(actor=request.user, target=target_permission)
+    action, result = conditionClient.change_condition(condition_pk=condition_id, permission_data=None, 
+        condition_data=reformatted_condition_data)
+    # FIXME: need to actually handle permission data
+
+    return JsonResponse({ "action_status": action.resolution.status, "action_log": action.resolution.log,
+        "new_display_name" : result.get_condition_type_class().descriptive_name })
