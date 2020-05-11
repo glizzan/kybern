166707e24669e429678634d7c44305c7a7998236
Shauna Gordon-McKeon <shaunagm@gmail.com>
2020-05-11 13:09:50 -0400

    Fix confusion between inbuilt and proxy User models in signals

--- kybern/accounts/models.py
+++ kybern/accounts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User as DjangoUser
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -24,12 +24,14 @@ class Profile(models.Model):
     bio = models.TextField(max_length=500, blank=True)
 
 
+@receiver(post_save, sender=DjangoUser)
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
+@receiver(post_save, sender=DjangoUser)
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
