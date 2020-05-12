114f9f6406479f4914a0472c4f3d81f13c4a84b1
Shauna Gordon-McKeon <shaunagm@gmail.com>
2020-05-12 16:43:24 -0400

    Update tests, fix some synch bugs with conditionals

--- kybern/tests.py
+++ kybern/tests.py
@@ -1,4 +1,4 @@
-import time
+import time, json
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from splinter import Browser
 from django.conf import settings
@@ -6,6 +6,9 @@ from selenium import webdriver
 
 from django.contrib.auth.models import User
 from concord.communities.client import CommunityClient
+from concord.permission_resources.client import PermissionResourceClient
+from concord.conditionals.client import PermissionConditionalClient
+from concord.actions.state_changes import Changes
 from groups.models import Group
 
 
@@ -56,11 +59,12 @@ class BaseTestCase(StaticLiveServerTestCase):
                 return True
         return False
 
-    def select_from_multiselect(self, selection, element_css=".multiselect__element"):
+    def select_from_multiselect(self, selection, element_css=".multiselect__element", search_within=None):
         """Helper method to select options given the custom interface vue-multiselect provides."""
-        self.browser.find_by_css(".multiselect__select").first.click()
+        base = search_within if search_within else self.browser
+        base.find_by_css(".multiselect__select").first.click()
         time.sleep(.25)
-        for item in self.browser.find_by_css(element_css):
+        for item in base.find_by_css(element_css):
             if selection in item.text:
                 item.click()
                 return True
@@ -224,6 +228,279 @@ class PermissionsTestCase(BaseTestCase):
         self.delete_selected_in_multiselect("crystaldunn")
         self.browser.find_by_id('save_member_changes').first.click()
         time.sleep(.5)
-        self.assertTrue(self.browser.is_text_present('action did not meet any permission criteria'))
+        self.assertTrue(self.browser.is_text_present('You do not have permission to take this action.'))
         self.browser.find_by_css(".close").first.click()  # close modal
         self.assertEquals(self.browser.find_by_id('members_member_count').text, "7 people")
+
+
+class ActionsTestCase(BaseTestCase):
+
+    def setUp(self):
+        self.create_users()
+        self.actor = User.objects.first()
+        self.client = CommunityClient(actor=self.actor)
+        self.client.community_model = Group
+        self.community = self.client.create_community(name="USWNT")
+        self.client.set_target(target=self.community)
+        self.client.add_members(member_pk_list=[user.pk for user in User.objects.all()])
+
+    def test_taking_action_generates_action(self):
+
+        # Add role
+        self.login_user("meganrapinoe", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_css(".role_name_display")[0].scroll_to()
+        self.browser.find_by_id('add_role_button').first.click()
+        self.browser.fill('role_name', 'forwards')
+        self.browser.find_by_id('save_role_button').first.click()
+        self.browser.find_by_css(".close").first.click()  # close modal
+
+        # Check for action in action history
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('meganrapinoe added role forwards to USWNT'))
+
+
+class ActionConditionsTestCase(BaseTestCase):
+
+    def setUp(self):
+
+        # Basic setup
+        self.create_users()
+        self.actor = User.objects.first()
+        self.client = CommunityClient(actor=self.actor)
+        self.client.community_model = Group
+        self.community = self.client.create_community(name="USWNT")
+        self.client.set_target(target=self.community)
+        self.client.add_members(member_pk_list=[user.pk for user in User.objects.all()])
+        self.client.add_role(role_name="forwards")
+        pinoe = User.objects.get(username="meganrapinoe")
+        press = User.objects.get(username="christenpress")
+        heath = User.objects.get(username="tobinheath")
+        self.client.add_people_to_role(role_name="forwards", people_to_add=[pinoe.pk, press.pk, heath.pk])
+
+        # Permission setup
+        self.permissionClient = PermissionResourceClient(actor=self.actor, target=self.community)
+        action, self.permission = self.permissionClient.add_permission(
+            permission_type = Changes.Communities.AddRole, permission_roles=["forwards"])
+
+    def test_adding_condition_to_permission_generates_condition(self):
+
+        # Pinoe adds condition to permission
+        self.login_user("meganrapinoe", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_id('forwards_editrole')[0].scroll_to()
+        self.browser.find_by_id('forwards_editrole').first.click()
+        permissions = [item.text for item in self.browser.find_by_css(".permission-display")]
+        self.assertEquals(permissions, ["add role to community"])
+        css_selector = "#permission_element_" + str(self.permission.pk) + " > div > button.btn.btn-secondary"
+        self.browser.find_by_css(css_selector).first.click()
+        self.browser.select("condition_select", "VoteCondition")
+        # TODO: look up - is there really no way to better identify vue-multiselect items?
+        element_containing_role_dropdown = self.browser.find_by_css(".permissionrolefield")[0]
+        self.select_from_multiselect("forwards", search_within=element_containing_role_dropdown)
+        self.browser.find_by_id('save_condition_button').first.click()
+        time.sleep(.25)
+        self.browser.find_by_css(".close").first.click()  # close modal
+
+        # Someone with the permission tries to take action (use asserts to check for condition error text)
+        self.login_user("christenpress", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_id('add_role_button').first.click()
+        self.browser.fill('role_name', 'midfielders')
+        self.browser.find_by_id('save_role_button').first.click()
+        time.sleep(.25)
+        self.assertTrue(self.browser.is_text_present('This action cannot be completed until a condition is passed.'))
+        self.browser.find_by_css(".close").first.click()  # close modal
+
+        # Go to action history and the condition link is there in the has_condition column   
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('christenpress asked to add role midfielders to USWNT')) 
+        self.browser.find_by_xpath('//*[@id="action_history_table_element"]/tbody/tr[1]/td[7]/button').first.click()
+        self.assertTrue(self.browser.is_text_present('Please cast your vote'))
+
+
+class ApprovalConditionsTestCase(BaseTestCase):
+
+    def setUp(self):
+
+        # create group, add members, add roles, add members to role
+        self.create_users()
+        self.actor = User.objects.first()
+        self.client = CommunityClient(actor=self.actor)
+        self.client.community_model = Group
+        self.community = self.client.create_community(name="USWNT")
+        self.client.set_target(target=self.community)
+        self.client.add_members(member_pk_list=[user.pk for user in User.objects.all()])
+        self.client.add_role(role_name="forwards")
+        pinoe = User.objects.get(username="meganrapinoe")
+        press = User.objects.get(username="christenpress")
+        heath = User.objects.get(username="tobinheath")
+        self.client.add_people_to_role(role_name="forwards", people_to_add=[pinoe.pk, press.pk, heath.pk])
+
+        # add permission & condition to permission
+        self.permissionClient = PermissionResourceClient(actor=self.actor, target=self.community)
+        action, self.permission = self.permissionClient.add_permission(
+            permission_type = Changes.Communities.AddRole, permission_roles=["forwards"])        
+        self.conditionClient = PermissionConditionalClient(actor=self.actor, target=self.permission)
+        action, self.condition = self.conditionClient.add_condition(condition_type="approvalcondition",
+            permission_data=json.dumps({ "approve_roles": ["forwards"], "reject_roles": ["forwards"] }))
+            
+        # have person take action that triggers permission/condition
+        self.client.set_actor(heath)
+        self.client.add_role(role_name="midfielders")
+
+    def test_approve_implements_action(self):
+
+        # User navigates to action history and approves action
+        self.login_user("christenpress", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('tobinheath asked to add role midfielders to USWNT')) 
+        self.browser.find_by_xpath('//*[@id="action_history_table_element"]/tbody/tr[1]/td[7]/button').first.click()
+        self.assertTrue(self.browser.is_text_present('Please approve or reject this action.'))  
+        self.browser.find_by_css("#btn-radios-1 > label:nth-child(1) > span").first.click()
+        time.sleep(.25)
+        self.browser.find_by_id('save_approve_choice').first.click()
+        time.sleep(.25)
+        self.assertTrue(self.browser.is_text_present("You have approved tobinheath's action. Nothing further is needed from you.")) 
+
+        # Navigate back to action history and check action is implemented
+        xpath_string = '//*[@id="action_history_modal_' + str(self.community.pk) + '_group___BV_modal_footer_"]/button[2]'
+        self.browser.find_by_xpath(xpath_string).first.click()
+        element = self.browser.find_by_css("#action_history_table_element > tbody > tr:nth-child(1) > td:nth-child(4)")[0]
+        self.assertTrue(element.text, "implemented")
+
+    def test_reject_rejects_action(self):
+        
+        # User navigates to action history and approves action
+        self.login_user("christenpress", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('tobinheath asked to add role midfielders to USWNT')) 
+        self.browser.find_by_xpath('//*[@id="action_history_table_element"]/tbody/tr[1]/td[7]/button').first.click()
+        self.assertTrue(self.browser.is_text_present('Please approve or reject this action.'))  
+        self.browser.find_by_css("#btn-radios-1 > label:nth-child(2) > span").first.click()
+        self.browser.find_by_id('save_approve_choice').first.click()
+        time.sleep(.25)
+        self.assertTrue(self.browser.is_text_present("You have rejected tobinheath's action. Nothing further is needed from you.")) 
+
+        # Navigate back to action history and check action is implemented
+        xpath_string = '//*[@id="action_history_modal_' + str(self.community.pk) + '_group___BV_modal_footer_"]/button[2]'
+        self.browser.find_by_xpath(xpath_string).first.click()
+        element = self.browser.find_by_css("#action_history_table_element > tbody > tr:nth-child(1) > td:nth-child(4)")[0]
+        self.assertTrue(element.text, "rejected")
+
+    def test_person_without_permission_to_approve_cant_approve(self):
+        self.login_user("emilysonnett", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('tobinheath asked to add role midfielders to USWNT')) 
+        self.browser.find_by_xpath('//*[@id="action_history_table_element"]/tbody/tr[1]/td[7]/button').first.click()
+        self.assertTrue(self.browser.is_text_present('You do not have permission to approve or reject this action.'))
+
+
+class VotingConditionTestCase(BaseTestCase):
+
+    def setUp(self):
+
+        # create group, add members, add roles, add members to role
+        self.create_users()
+        self.actor = User.objects.first()
+        self.client = CommunityClient(actor=self.actor)
+        self.client.community_model = Group
+        self.community = self.client.create_community(name="USWNT")
+        self.client.set_target(target=self.community)
+        self.client.add_members(member_pk_list=[user.pk for user in User.objects.all()])
+        self.client.add_role(role_name="forwards")
+        pinoe = User.objects.get(username="meganrapinoe")
+        press = User.objects.get(username="christenpress")
+        heath = User.objects.get(username="tobinheath")
+        self.client.add_people_to_role(role_name="forwards", people_to_add=[pinoe.pk, press.pk, heath.pk])
+
+        # add permission & condition to permission
+        self.permissionClient = PermissionResourceClient(actor=self.actor, target=self.community)
+        action, self.permission = self.permissionClient.add_permission(
+            permission_type = Changes.Communities.AddRole, permission_roles=["forwards"])        
+        self.conditionClient = PermissionConditionalClient(actor=self.actor, target=self.permission)
+        action, self.condition = self.conditionClient.add_condition(condition_type="votecondition",
+            permission_data=json.dumps({ "vote_roles": ["forwards"] }))
+            
+        # have person take action that triggers permission/condition
+        self.client.set_actor(heath)
+        self.client.add_role(role_name="midfielders")
+
+    def test_yea_updates_vote_results(self):
+
+        # User navigates to action history and votes yea
+        self.login_user("christenpress", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('tobinheath asked to add role midfielders to USWNT')) 
+        self.browser.find_by_xpath('//*[@id="action_history_table_element"]/tbody/tr[1]/td[7]/button').first.click()       
+        self.assertTrue(self.browser.is_text_present('The results so far are 0 yeas and 0 nays with 0 abstentions.'))
+        self.assertTrue(self.browser.is_text_present('Please cast your vote'))  
+        self.browser.find_by_css("#btn-radios-1 > label:nth-child(1) > span").first.click()
+        time.sleep(.25)
+        self.browser.find_by_id('save_vote_choice').first.click()
+        time.sleep(.25)
+        time.sleep(2)
+        self.assertTrue(self.browser.is_text_present('The results so far are 1 yeas and 0 nays with 0 abstentions.'))
+        self.assertTrue(self.browser.is_text_present("Thank you for voting! No further action from you is needed.")) 
+
+    def test_nay_updates_vote_results(self):
+
+        # User navigates to action history and votes nay
+        self.login_user("christenpress", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('tobinheath asked to add role midfielders to USWNT')) 
+        self.browser.find_by_xpath('//*[@id="action_history_table_element"]/tbody/tr[1]/td[7]/button').first.click()       
+        self.assertTrue(self.browser.is_text_present('The results so far are 0 yeas and 0 nays with 0 abstentions.'))
+        self.assertTrue(self.browser.is_text_present('Please cast your vote'))  
+        self.browser.find_by_css("#btn-radios-1 > label:nth-child(2) > span").first.click()
+        time.sleep(.25)
+        self.browser.find_by_id('save_vote_choice').first.click()
+        time.sleep(.25)
+        self.assertTrue(self.browser.is_text_present('The results so far are 0 yeas and 1 nays with 0 abstentions.'))
+        self.assertTrue(self.browser.is_text_present("Thank you for voting! No further action from you is needed.")) 
+
+    def test_abstain_updates_vote_results(self):
+        
+        # User navigates to action history and votes nay
+        self.login_user("christenpress", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('tobinheath asked to add role midfielders to USWNT')) 
+        self.browser.find_by_xpath('//*[@id="action_history_table_element"]/tbody/tr[1]/td[7]/button').first.click()       
+        self.assertTrue(self.browser.is_text_present('The results so far are 0 yeas and 0 nays with 0 abstentions.'))
+        self.assertTrue(self.browser.is_text_present('Please cast your vote'))  
+        self.browser.find_by_css("#btn-radios-1 > label:nth-child(3) > span").first.click()
+        time.sleep(.25)
+        self.browser.find_by_id('save_vote_choice').first.click()
+        time.sleep(.25)
+        self.assertTrue(self.browser.is_text_present('The results so far are 0 yeas and 0 nays with 1 abstentions.'))
+        self.assertTrue(self.browser.is_text_present("Thank you for voting! No further action from you is needed.")) 
+
+    def test_person_without_permission_to_approve_cant_vote(self):
+
+        self.login_user("emilysonnett", "badlands2020")
+        self.go_to_group("USWNT")
+        self.browser.find_by_css("#action_history > span > button")[0].scroll_to()
+        self.browser.find_by_css("#action_history > span > button").first.click()
+        self.assertTrue(self.browser.is_text_present('tobinheath asked to add role midfielders to USWNT')) 
+        self.browser.find_by_xpath('//*[@id="action_history_table_element"]/tbody/tr[1]/td[7]/button').first.click()
+        self.assertTrue(self.browser.is_text_present('You are not eligible to vote.'))
+
+
+    # def test_vote_has_passed(self):
+    #     # FIXME: not sure how to do this, given the one hour vote minimum?
+    #     # maybe the vote needs a: [close when X people voted option]
+    #     ...
