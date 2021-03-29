25c705f61ce6e690c8150c5e6ee68936adc56abd
Shauna Gordon-McKeon <shaunagm@gmail.com>
2021-03-29 12:22:36 -0400

    Make groups page same as landing page, with info minimized

--- kybern/accounts/views.py
+++ kybern/accounts/views.py
@@ -22,6 +22,7 @@ class IndexView(generic.TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["groups"] = Group.objects.all()
+        context["hide_info"] = True if self.request.GET.get("hide_info", None) else False
         return context
 
 
